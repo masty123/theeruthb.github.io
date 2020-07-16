adfcd471c48cd0f55eae9d73cd91da7a543dd5a0
masty123 <theeruth.b@ku.th>
2020-07-16 18:58:51 +0700

    adjust scroll duration

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px' ,
-    duration: 1000,
+    duration: 2000,
     reset: 'true'
 })
 
