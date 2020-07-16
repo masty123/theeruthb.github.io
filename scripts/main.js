51693dcaa9809912bcd86569557c4d0320f0b24c
masty123 <pwnimatorz@hotmail.com>
2020-07-16 14:40:14 +0700

    fixing mobile view

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,27 @@
-/* Menu Show */
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    nav = document.getElementById(navId);
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
     if(toggle && nav){
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
         })
     }
 }
+showMenu('nav-toggle','nav-menu')
 
-showMenu('nav-toggle','nav-menu');
-
-/* Active and remove menu */
-const navLink = document.querySelectorAll('.nav_link');
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link');   
 
 function linkAction(){
-    //Active Link
-    navLink.forEach(n => n.classList.remove('active'));
-    this.classList.add('active');
-
-    //Remove mobile menu
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show');
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
